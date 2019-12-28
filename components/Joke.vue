56a288bb57c30947e73930adb87adaf6cac0831f
RavalVarun <44644972+RavalVarun@users.noreply.github.com>
2019-12-28 16:18:01 -0500

    render jokes on jokes page

--- components/Joke.vue
+++ components/Joke.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="joke">
-        <p>{{ joke }}</p>
-    </div>
+    <nuxt-link :to="'jokes/' + id">
+        <div class="joke">
+            <p>{{ joke }}</p>
+        </div>
+    </nuxt-link>
 </template>
 
 <script>
 export default {
-    name: 'Joke',
-    props: ['joke', 'id']
-}
+    name: "Joke",
+    props: ["joke", "id"]
+};
 </script>
 
 <style>
